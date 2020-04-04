1abce07e0ba59fe5f61e47265b555b88e360fa5a
Xiaotian Duan <xduan7@gmail.com>
2020-04-04 09:19:22 -0500

    change directory name from processes to data_processes and create dataset file

---
+++ src/data_processes/dataset.py
@@ -0,0 +1,16 @@
+"""
+File Name:          dataset.py
+Project:            omniglot-experiments
+
+File Description:
+
+    references:
+    - https://github.com/brendenlake/omniglot
+    - https://github.com/pytorch/vision/blob/master/torchvision/datasets/omniglot.py
+    - https://github.com/dragen1860/MAML-Pytorch/blob/master/omniglot.py
+
+    From the fist reference, it seems that there are multiple learning
+    problems. The main one being training on background and testing on
+    evaluation, and there are smaller and harder training sets.
+
+"""
